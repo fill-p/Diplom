c45ed87206cafe8c50e739dfc065f107277a1de0
fill-p <83204197+fill-p@users.noreply.github.com>
2022-06-06 10:33:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -58,74 +58,74 @@ function StringSearch() {
 
   if (SearchTermTest1Condition.length > 0 && TextSearch.indexOf(SearchTermTest1Condition) > -1) {
     if (SearchTermTest1.length > 0 && TextSearch.indexOf(SearchTermTest1) > -1) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test1').value = 'Завдання 1: Корінь з допомогою Math - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test1').value = 'Завдання 1: Корінь з допомогою Math - не зараховано';
     }
   } else if (SearchTermTest2Condition.length > 0 && TextSearch.indexOf(SearchTermTest2Condition) > -1 ) {
     if (SearchTermTest2Option1.length > 0 && TextSearch.indexOf(SearchTermTest2Option1) > -1 || TextSearch.indexOf(SearchTermTest2Option2) > -1) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test2').value = 'Завдання 2: Квадрат числа з допомогою Math - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test2').value = 'Завдання 2: Квадрат числа з допомогою Math - не зараховано';
     }
   } else if (SearchTermTest3Condition.length > 0 && TextSearch.indexOf(SearchTermTest3Condition) > -1) {
     if (SearchTermTest3Option1.length > 0 && TextSearch.indexOf(SearchTermTest3Option1) > -1 || TextSearch.indexOf(SearchTermTest3Option2) > -1) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test3').value = 'Завдання 3: Число в степені числа з допомогою Math - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test3').value = 'Завдання 3: Число в степені числа з допомогою Math - не зараховано';
     }
   } else if (SearchTermTest4Condition.length > 0 && TextSearch.indexOf(SearchTermTest4Condition) > -1) {
     if (SearchTermTest4Option1.length > 0 && TextSearch.indexOf(SearchTermTest4Option1) > -1 || TextSearch.indexOf(SearchTermTest4Option2) > -1) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test4').value = 'Завдання 1: Цикл for - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test4').value = 'Завдання 1: Цикл for - не зараховано';
     }
   } else if (SearchTermTest5Condition.length > 0 && TextSearch.indexOf(SearchTermTest5Condition) > -1) {
     if (TextSearch.indexOf(SearchTermTestIterator) > -1 && (TextSearch.indexOf(SearchTermTest5Option1) > -1 || TextSearch.indexOf(SearchTermTest5Option2) > -1)) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test5').value = 'Завдання 2: Цикл while - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test5').value = 'Завдання 2: Цикл while - не зараховано';
     }
   } else if (SearchTermTest6Condition.length > 0 && TextSearch.indexOf(SearchTermTest6Condition) > -1) {
     if (TextSearch.indexOf(SearchTermTestIterator) > -1 && (TextSearch.indexOf(SearchTermTest6Option1) > -1 || TextSearch.indexOf(SearchTermTest6Option2) > -1)) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test6').value = 'Завдання 3: Цикл do while - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test6').value = 'Завдання 3: Цикл do while - не зараховано';
     }
   } else if (SearchTermTest7Condition.length > 0 && TextSearch.indexOf(SearchTermTest7Condition) > -1) {
     if ((TextSearch.indexOf(SearchTermTest7Option1) > -1 && TextSearch.indexOf(SearchTermTest7Option2) > -1) || (TextSearch.indexOf(SearchTermTest7Option3) > -1 && TextSearch.indexOf(SearchTermTest7Option4) > -1)) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test7').value = 'Завдання 1: Зміна тексту в input - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test7').value = 'Завдання 1: Зміна тексту в input - не зараховано';
     }
   } else if (SearchTermTest8Condition.length > 0 && TextSearch.indexOf(SearchTermTest8Condition) > -1) {
     if ((TextSearch.indexOf(SearchTermTest7Option1) > -1 && TextSearch.indexOf(SearchTermTest8Option1) > -1) || (TextSearch.indexOf(SearchTermTest7Option3) > -1 && TextSearch.indexOf(SearchTermTest8Option2) > -1)) {
-      swal("Вірно!", "", "success");
+      swal("Правильно!", "", "success");
       document.getElementById('test8').value = 'Завдання 2: Зміна кольору в input - зараховано';
       i++;
     } else {
-      swal("Не вірно!", "", "error");
+      swal("Не правильно!", "", "error");
       document.getElementById('test8').value = 'Завдання 2: Зміна кольору в input - не зараховано';
     }
   } else {
